2d2611d16d7ad6e9d6e17587cedcc8694f133fa3
Adrian Tame Jacobo <adriantj@sphynx.local>
2021-12-04 19:37:54 -0600

    Agregando todos los curls

--- app/addvalues.sh
+++ app/addvalues.sh
@@ -1,3 +1,6 @@
+# Agregar base de datos de forma inicial
+docker exec web_miguel_adrian curl -X POST -H "Content-Type: application/json" -d @app/datos_json.txt 0.0.0.0:8080/users
+
 # Ejemplo de POST para un solo user: 
 curl -X POST -H "Content-Type: application/json"\
      -d '{"gender": 1, "age": 67, "hypertension": 0, "heart_disease": 1, "ever_married": 1, "Residence_type": 1, "avg_glucose_level": "228.69", "bmi": "36.6", "smoking_status": 2, "stroke": 1}'\
@@ -11,7 +14,7 @@ curl '0.0.0.0:8080/user?id=3'
 
 # Ejemplo de PATCH para user: 
 curl -X PATCH -H "Content-Type: application/json"\
-     -d '{"gender": 0, "age": 670, "hypertension": 0, "heart_disease": 1, "ever_married": 1, "Residence_type": 1, "avg_glucose_level": "228.69", "bmi": "36.6", "smoking_status": 2, "stroke": 1}'\
+     -d '{"gender": 0, "age": 6700, "hypertension": 0, "heart_disease": 1, "ever_married": 1, "Residence_type": 1, "avg_glucose_level": "228.69", "bmi": "36.6", "smoking_status": 2, "stroke": 1}'\
      '0.0.0.0:8080/user?id=4'
 
 # Ejemplo de GET para users (regresa toda la base de datos): 
@@ -25,5 +28,58 @@ curl -X POST -H "Content-Type: application/json"\
 
 # Ejemplo de DELETE para múltiples users: 
 curl -X DELETE -H "Content-Type: application/json"\
-     -d '{"id": [7,8]}'\
+     -d '{"id": [1,2,3,4,5,6,7,8]}'\
      '0.0.0.0:8080/users'
+
+# Ejemplo de Predicción (cuidado de checar la fecha!)
+curl -X GET -H "Content-Type: application/json"\
+     -d '{ 
+	"user":
+			{
+					"gender": 1, 
+					"age": 2, 
+					"hypertension": 0, 
+					"heart_disease": 1, 
+					"ever_married": 1, 
+					"Residence_type": 1, 
+					"avg_glucose_level": "1000.69", 
+					"bmi": "36.6",
+					"smoking_status": 1
+			},
+
+	"model":
+			{
+					"version": "modelos_locales",
+					"year": "2021",
+					"month": "12",
+					"day": "05",
+					"hour": "01",
+					"minute": "23",
+					"second": "10"
+			}
+}'\
+     '0.0.0.0:8080/predict'
+
+# Ejemplo de Predicción con el modelo dentro del docker más reciente
+curl -X GET -H "Content-Type: application/json"\
+     -d '{ 
+	"user":{"gender": 1, "age": 2, "hypertension": 0, "heart_disease": 1, "ever_married": 1, "Residence_type": 1, "avg_glucose_level": "1000.69", "bmi": "36.6","smoking_status": 1},
+	"model": {"version": "latest_local"} }'\
+     '0.0.0.0:8080/predict'     
+
+# Ejemplo de Predicción con el modelo fuera de docker más reciente (Carpeta /app/modelos)
+curl -X GET -H "Content-Type: application/json"\
+     -d '{ 
+	"user":{"gender": 1, "age": 2, "hypertension": 0, "heart_disease": 1, "ever_married": 1, "Residence_type": 1, "avg_glucose_level": "1000.69", "bmi": "36.6","smoking_status": 1},
+	"model": {"version": "latest"} }'\
+     '0.0.0.0:8080/predict'
+
+# Salvar un modelo específico afuera de docker.
+
+curl -X POST -H "Content-Type: application/json"\
+     -d '{"year": "2021","month": "12","day": "05","hour": "01","minute": "23","second": "10"}' \
+     '0.0.0.0:8080/save_model'
+
+
+# Salvar el modelo más reciente a modelos.
+curl '0.0.0.0:8080/save_model'
--- app/app.py
+++ app/app.py
@@ -19,6 +19,7 @@ import random
 import io
 
 import os
+import shutil
 from datetime import datetime
 from joblib import dump
 from joblib import load
@@ -30,76 +31,40 @@ database_uri = "postgresql://postgres:postgres@db:5432/postgres"
 app = Flask(__name__)
 conn = psycopg2.connect(database_uri)
 
+
+
+
+
+month_dict = {"01" : 'Enero',
+              "02" : 'Febrero',
+              "03" : 'Marzo',
+              "04" : 'Abril',
+              "05" : 'Mayo',
+              "06" : 'Junio',
+              "07" : 'Julio',
+              "08" : 'Agosto',
+              "09" :'Septiembre',
+              "10" : 'Octubre',
+              "11" :'Noviembre',
+              "12" : 'Diciembre'}
+def format_file(file):
+    year = file.split('_')[-6]
+    month = file.split('_')[-5]
+    day = file.split('_')[-4]
+    hour = file.split('_')[-3]
+    minute = file.split('_')[-2]
+    second = file.split('_')[-1][:2]
+    
+    return month_dict[month] + ' ' + day + ', ' + year + ' (' + hour + 'H:' + minute + 'M::' + second + 'S)'
+
+
+
+
+
+
 @app.route("/")
 def home():
     return render_template('index.html')
-
-@app.route("/train_model")
-def train_model():
-    cur = conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
-    cur.execute("select * from users")
-    results = cur.fetchall()
-    cur.close()
-    print("Data has been retreived. Model is being trained...")
-    all_data = pd.DataFrame(results, columns = ['id', 'gender', 'age', 'hypertension', 'heart_disease', \
-            'ever_married', 'Residence_type', 'avg_glucose_level', 'bmi', 'smoking_status', 'stroke'])
-    all_data.drop('id', 1, inplace = True)
-
-    X_train, X_test, y_train, y_test = train_test_split(all_data.drop(columns = "stroke"),
-                                                        all_data['stroke'],
-                                                        random_state = 203129)
-
-    param_grid = {'n_estimators': [100, 120, 140],
-                  'max_features': [5, 7, 9],
-                 }
-
-    grid = GridSearchCV(
-            estimator  = RandomForestClassifier(random_state = 203129),
-            param_grid = param_grid,
-            scoring    = 'neg_log_loss',
-            n_jobs     = multiprocessing.cpu_count() - 1,
-            cv         = RepeatedKFold(n_splits=5, n_repeats=3, random_state=123), 
-            refit      = True,
-            verbose    = 0,
-            return_train_score = True
-        )
-
-    grid.fit(X = X_train, y = y_train)
-
-    modelo_final = grid.best_estimator_
-    predicciones = modelo_final.predict(X = X_test)
-
-    id_model = str(datetime.now())[:-7].replace('-', '_').replace(' ', '_').replace(':', '_')
-    dump(modelo_final, filename = f"/app/modelos_locales/random_forest_{id_model}.joblib")    
-
-    return json.dumps({"message": 'El modelo fue entrenado y guardado exitosamente.'})
-
-@app.route("/predict")
-def predict():
-    aux = [file for file in os.listdir('/app/modelos_locales') if file[-6:] == 'joblib']
-    if len(aux) > 0:
-        modelo_actual = load('/app/modelos_locales/' + aux[-1])
-        
-        new_user_json = {
-                        "gender": 1, 
-                        "age": 67, 
-                        "hypertension": 0, 
-                        "heart_disease": 1, 
-                        "ever_married": 1, 
-                        "Residence_type": 1, 
-                        "avg_glucose_level": "278.69", 
-                        "bmi": "36.6",
-                        "smoking_status": 2
-                        }
-        
-        new_user = np.array([pd.Series(new_user_json)])
-        
-        if modelo_actual.predict(new_user) == 1:
-            return json.dumps({"message": 'Tienes riesgo de sufrir un derrame.'})
-        else:
-            return json.dumps({"message": 'NO tienes riesgo de sufrir un derrame.'})
-    else:
-        return json.dumps({"message":'Se necesita un modelo. Prueba /train_model.'})
 
 @app.route("/user", methods=["POST", "GET", "DELETE", "PATCH"])
 def user():
@@ -172,9 +137,121 @@ def users():
         cur.close()
         return json.dumps({"mensaje": 'Se borraron correctamente'})
 
-@app.route("/save_model")
-def guardar():
-    modelo = request.json
+@app.route("/train_model")
+def train_model():
+    param_grid = request.json 
+    cur = conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+    cur.execute("select * from users")
+    results = cur.fetchall()    
+    cur.close()
+    print("Data has been retreived. Model is being trained...")
+    all_data = pd.DataFrame(results, columns = ['id', 'gender', 'age', 'hypertension', 'heart_disease', \
+            'ever_married', 'Residence_type', 'avg_glucose_level', 'bmi', 'smoking_status', 'stroke'])
+    all_data.drop('id', 1, inplace = True)
+
+    X_train, X_test, y_train, y_test = train_test_split(all_data.drop(columns = "stroke"),
+                                                        all_data['stroke'],
+                                                        random_state = 203129)
+
+    param_grid = {'n_estimators': [100, 120, 140],
+                  'max_features': [5, 7, 9],
+                 }
+
+    param_grid = {'n_estimators': [120],
+                  'max_features': [5],
+                 }
+
+    grid = GridSearchCV(
+            estimator  = RandomForestClassifier(random_state = 203129),
+            param_grid = param_grid,
+            scoring    = 'neg_log_loss',
+            n_jobs     = multiprocessing.cpu_count() - 1,
+            cv         = RepeatedKFold(n_splits=5, n_repeats=3, random_state=203129), 
+            refit      = True,
+            verbose    = 0,
+            return_train_score = True
+        )
+
+    grid.fit(X = X_train, y = y_train)
+
+    modelo_final = grid.best_estimator_
+    predicciones = modelo_final.predict(X = X_test)
+
+    id_model = str(datetime.now())[:-7].replace('-', '_').replace(' ', '_').replace(':', '_')
+    dump(modelo_final, filename = f"/app/modelos_locales/random_forest_{id_model}.joblib")    
+
+    return json.dumps({"message": 'El modelo fue entrenado y guardado exitosamente.'})
+
+@app.route("/predict")
+def predict():
+
+    datos = request.json
+    new_user = datos['user']
+    modelo = datos['model']
+    version = modelo['version']
+
+    ### Obtenemos el modelo de acuerdo a lo contenido en el json
+    if len(modelo) == 1: ### Caso para usar el último modelo
+        if version == 'latest':
+            aux = [file for file in os.listdir('/app/modelos') if file[-6:] == 'joblib']
+            if len(aux) > 0:
+                modelo_actual = load('/app/modelos/' + aux[-1])
+            else:
+                return json.dumps({"message":'Se necesita un modelo. Prueba /train_model, y guárdalo con save_model.'})
+
+        elif version == 'latest_local':
+            aux = [file for file in os.listdir('/app/modelos_locales') if file[-6:] == 'joblib']
+            if len(aux) > 0:
+                modelo_actual = load('/app/modelos_locales/' + aux[-1])
+            else:
+                return json.dumps({"message":'Se necesita un modelo. Prueba /train_model.'})
+        else:
+            return json.dumps({"message":'Opción no valida. Elegir "latest", "latest_local", o pasar un json con las especificaciones.'})
+
+    else: ### Caso para especificar cual modelo se quiere usar
+            
+        selected_model_string = ''
+        for s in list(modelo.values())[1:]:
+            selected_model_string += s + '_'
+        selected_model_string = selected_model_string[:-1]
+
+        aux = [file for file in os.listdir(f'/app/{version}') if file[-6:] == 'joblib']
+        if len(aux) > 0:
+            try:
+                modelo_actual = load(f'app/{version}/random_forest_{selected_model_string}.joblib')              
+            except FileExistsError:
+                return json.dumps({"message":'El modelo no existe, verifica que los datos de la fecha y tiempo sean correctos.'})    
+        else:
+            return json.dumps({"message":'Se necesita un modelo. No hay modelos guardados.'})
+
+    new_user = np.array([pd.Series(new_user)])
+        
+    ### Predecimos con el modelo seleccionado
+    if modelo_actual.predict(new_user) == 1:
+        return json.dumps({"message": 'Tienes riesgo de sufrir un derrame.'})
+    else:
+        return json.dumps({"message": 'NO tienes riesgo de sufrir un derrame.'})
+
+@app.route("/save_model", methods=["POST", "GET"])
+def save_model():
+    if request.method == "POST":
+        modelo = request.json
+        try:
+            selected_model_string = ''
+            for s in modelo.values():
+                selected_model_string += s + '_'
+            selected_model_string = selected_model_string[:-1]
+            shutil.copy(f'app/modelos_locales/random_forest_{selected_model_string}.joblib', f'app/modelos/random_forest_{selected_model_string}.joblib')
+            return json.dumps({"message":'El modelo se guardó corectamente.'})
+        except FileNotFoundError:
+            return json.dumps({"message":'El modelo no existe.'})
+    if request.method == "GET":
+        aux = [file for file in os.listdir('/app/modelos_locales') if file[-6:] == 'joblib']
+        if len(aux) > 0:
+            shutil.copy(f'app/modelos_locales/' + aux[-1], f'app/modelos/' + aux[-1])
+            return json.dumps({"message":'El modelo se guardo corectamente.'})
+        else:
+            return json.dumps({"message":'Se necesita un modelo. Prueba /train_model.'})
 
 @app.route("/show_models")
 def show_models():
@@ -182,7 +259,7 @@ def show_models():
     if len(aux) == 0:
         return json.dumps({"message":'No hay modelos guardados'})
     else: 
-        return json.dumps(dict(aux))
+        return json.dumps({format_file(file):file for file in aux})
 
 @app.route("/show_local_models")
 def show_local_models():
@@ -190,7 +267,7 @@ def show_local_models():
     if len(aux) == 0:
         return json.dumps({"message":'No hay modelos locales guardados'})
     else: 
-        return json.dumps([x._asdict() for x in aux], default=str)
+        return json.dumps({format_file(file):file for file in aux})
 
 @app.route("/tabla_usuarios")
 def render_table():
